a9a165af11ef797d96fb0f442274fb5dee6ff1aa
MUHAMED <mohammedhashesh389@gmail.com>
2023-09-07 16:59:01 +0300

    delete package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:coffee_app/views/splash_screen.dart';
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const CoffeeApp());
